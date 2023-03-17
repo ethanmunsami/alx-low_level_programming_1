5dc88151d1aac8a8f6e54d9f0c434e510ba3b4e0
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-17 13:52:56 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,19 @@
 /*
- * File: 5-printf.c
- * Author: Ethan Munsami
+ *File: 5-printf.c
+ *Author: Ethan Munsami
  */
 
 #include <stdio.h>
 
 /**
- * main - entry point
- * Description: Function outputs "with proper grammar, 
- * but the outcome is a piece of art,"
- * Return: Always (0)
+ *main - entry point
+ *Description: Function outputs "with proper grammar, 
+ *but the outcome is a piece of art,"
+ *Return: Always (0)
  */
 int main(void)
 { 
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
