117b15ad3fb06e8348fb222f63388d7e9bd04798
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-17 13:25:57 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,18 +6,17 @@
 #include <stdio.h>
 
 /*
-* Main function prints "Programming is like building a multilingual puzzle, follwoed by a new line
+* Main function prints "Programming is like building a multilingual
+* puzzle, followed by a new line
 * Return always 0
 */
-
 int main(void)
-
-	{
+{
 	
-	char string[] = "\"Programming is like building a multilingual puzzle";
+	char string[] ="\"Programming is like building a multilingual puzzle";
 
 	puts(string);
 
 	return (0);
 
-	}
+}
