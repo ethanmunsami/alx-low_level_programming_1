5f732c12cd3fef9cc5ca446d18bb3fca5f722cc4
Etha Munsami <ethanmunsami@gmail.com>
2023-03-17 04:50:16 -0700

    Added and modified 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Author: Ethan Munsami
+ */
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Function outputs "with proper grammar, 
+ * but the outcome is a piece of art,"
+ * Return: Always (0)
+ */
+int main(void)
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
