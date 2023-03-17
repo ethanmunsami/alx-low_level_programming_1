d19ff4f214eba1638e53ef14369f2a2522379673
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-17 13:19:02 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@ int main(void)
 
 	{
 
-	char string[] = "Programming is like building a multilingual puzzle";
+	char string[] = """ + "Programming is like building a multilingual puzzle";
 
 	puts(string);
 
