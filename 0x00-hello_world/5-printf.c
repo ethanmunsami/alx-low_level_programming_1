70162e2be7654f4dd3d40b2ad63139c87cc9cb22
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-17 13:53:44 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,12 +7,12 @@
 
 /**
  *main - entry point
- *Description: Function outputs "with proper grammar, 
+ *Description: Function outputs "with proper grammar,
  *but the outcome is a piece of art,"
  *Return: Always (0)
  */
 int main(void)
-{ 
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
