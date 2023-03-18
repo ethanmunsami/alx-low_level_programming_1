b7402f3d0dc27aee51d282a46c4422da9dde2b30
Etha Munsami <ethanmunsami@gmail.com>
2023-03-18 07:37:57 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/*
+ * File: 101-quote.c
+ * Author: Ethan Munsami
+ */
+
+#include <stdio.h>
+/*
+ * Function: Outputs "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19"
+ * Return: (1)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return(1);
+}
