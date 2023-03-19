c41abf3e7cbe0f846e8074a3cd8df6577db6792f
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-19 19:19:34 +0200

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,13 +6,14 @@
 /**
 * main - entry point
 * Description: "Prints numbers of base 10"
+* Return: Always (0)
 */
 int main(void)
 {
   int n = 0;
-
-  while(n < 10)
-    {
-      printf("%d", n);
-    }
+  
+    for (n = 0; n < 10; n++)
+      printf("%d", n)
+      
+  return (0);
 }
