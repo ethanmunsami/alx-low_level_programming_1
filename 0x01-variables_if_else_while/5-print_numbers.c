62e19d6c569c94c44ce13161856fe8c829d4d367
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-19 19:15:35 +0200

    Update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/*
+* File: 5-print_numbers.c
+* Author: Ethan Munsami
+*/
+#include <stdio.h>
+/**
+* main - entry point
+* Description: "Prints numbers of base 10"
+*/
+int main(void)
+{
+  int n = 0;
+
+  while(n < 11)
+    {
+      printf(n);
+    }
+}
