b160ac2fd9078909b7f79e55f13e7f91e0444a8d
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-19 10:53:56 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,20 +12,14 @@
  */
 int main(void)
 {
-	int n = 100
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	} 
-	else
-	{
-	 if (n == 0)
-	 {
-		printf("%d is zero\n", n);
-	 }
-	 else
-	 {
-		printf("%d is negative\n", n);
-	 }
-	}
+ if (n < 0) 
+    {
+        printf("%d is negative\n", n);
+    }
+    else if (n == 0) {
+        printf("%d is zero\n", n);
+    }
+    else {
+        printf("%d is positive\n", n);
+    }
 }
