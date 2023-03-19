72c9b31642fb7006553b105ff7180777c3c4c3ce
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-19 19:40:05 +0200

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Ethan Munsami
+ */
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: "Prints all possible combinations of single-digit numbers"
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
