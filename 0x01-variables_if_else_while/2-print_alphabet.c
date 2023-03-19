4b56b44b2eb167ce2bf83ede4059ec85829383a0
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-19 18:05:01 +0200

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+* File: 2-print_alphabet.c
+* Author: Ethan Munsami
+*/
+
+#include <stdio.h>
+/**
+* main - entry point
+* Description: "Prints the alphabet in lower case"
+*
+*Return: Always (0)
+*/
+int main(void)
+{
+char ch = 'a';
+
+for (ch='a'; ch<='z'; ch++)
+{
+putchar(ch, " ");
+}
+return (0);
+}
