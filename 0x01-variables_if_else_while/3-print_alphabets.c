4760a99429e4fc044a174e9bb8634f46f76d2507
Etha Munsami <ethanmunsami@gmail.com>
2023-03-19 09:43:09 -0700

    Added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-print_alphabets.c
+ * Author: Ethan Munsami
+ */
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: "Prints alphabet in lowercase and uppercase"
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char ch2 = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
+	{
+		putchar(ch2);
+	}
+	putchar('\n');
+}
