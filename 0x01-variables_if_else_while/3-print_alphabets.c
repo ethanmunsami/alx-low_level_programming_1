425a644e69f3af3ee10224f0b701e12b65c511b7
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-19 18:45:33 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,15 +12,15 @@
 int main(void)
 {
 	char ch = 'a';
-	char ch2 = 'A';
+	/*char ch2 = 'A';*/
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
+	/*for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
 	{
 		putchar(ch2);
-	}
+	}*/
 	putchar('\n');
 }
