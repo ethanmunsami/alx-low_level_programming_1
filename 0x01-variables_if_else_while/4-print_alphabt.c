c12833d5e538840143b52a8b3ab8c69865dca8cd
Etha Munsami <ethanmunsami@gmail.com>
2023-03-19 10:01:40 -0700

    Added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_alphabt.c
+ * Author: Ethan Munsami
+ */
+#include <studio.h>
+/**
+ * main - entry point
+ * Description: "Prints the alphabet except q and e"
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+}
+
