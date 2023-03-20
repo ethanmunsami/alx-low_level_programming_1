f5a8cf36315f2c93063e6c008166db824979b217
Etha Munsami <ethanmunsami@gmail.com>
2023-03-20 04:11:27 -0700

    Additions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-putchar.c
+ * Author: Ethan Munsami
+ */
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: "Prints 'putchar'"
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
