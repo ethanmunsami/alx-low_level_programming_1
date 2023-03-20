aaae5b43a3030b17f57c3eed4f886ee08e5a89d3
Etha Munsami <ethanmunsami@gmail.com>
2023-03-20 06:36:20 -0700

    Additions

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-isalpha.c
+ * Author: Ethan Munsami
+ */
+
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: "Checks for alphabetic characters"
+ *
+ * Return: "1 if it's a char, 0 if not"
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
+}
