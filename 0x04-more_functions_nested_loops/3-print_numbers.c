e66627a9e4d3e1523174799dafbf2c0d558d29ac
Etha Munsami <ethanmunsami@gmail.com>
2023-03-23 06:15:35 -0700

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function prints numbers from 0 to 9,
+ * and a new line
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	printf("\n");
+}
