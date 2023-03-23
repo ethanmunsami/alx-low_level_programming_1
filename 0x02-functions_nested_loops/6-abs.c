d3617ffb64219e84a24851504a41a6fecb1887f6
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-23 17:14:20 +0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,32 +1,25 @@
-/*
- * File: 6-abs.c
- * Author: Ethan Munsami
- */
-
 #include "main.h"
-#include <stdio.h>
-
 
 /**
-  * print_sign - "Prints the sign of a number"
-  *
-  * Return: 1 for positive num, -1 for negative num or 0 for anything else
+  * print_sign - Prints the sign of a number
+  * @n: The number to be checked
+  * Return: 1 for positive num, -1 for negative num or zero for anything else
   */
-int _abs(int n)
+int print_sign(int n)
 {
-	int x;
-	
 	if (n > 0)
 	{
-		printf("%d\n", n);
+		_putchar(43);
+		return (1);
 	}
 	else if (n < 0)
 	{
-		x = (-1) * n;
-		printf("%d\n", x);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
+		_putchar(48);
 		return (0);
 	}
 }
