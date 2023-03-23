b20da6600ed17d436406364b6641a8eced7e1ac5
Etha Munsami <ethanmunsami@gmail.com>
2023-03-23 06:56:05 -0700

    Added 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 0 to 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	char ch = '0';
+
+	while (i <= 10)
+	{
+		for (ch = '0'; ch <= '14'; ch++)
+		{
+			_putchar(ch);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
