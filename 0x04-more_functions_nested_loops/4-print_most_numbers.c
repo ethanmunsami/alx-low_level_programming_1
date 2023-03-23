0f4175bb20b8709724087bdc8bdc3a40675aea55
Etha Munsami <ethanmunsami@gmail.com>
2023-03-23 06:38:48 -0700

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			printf("%d", i);
+	}
+}
