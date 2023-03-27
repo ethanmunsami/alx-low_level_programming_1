15cc7e3b25c0bc41956c787ec80f31c749833659
ethanmunsami <125829979+ethanmunsami@users.noreply.github.com>
2023-03-27 13:31:05 +0200

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _puts - Prints a string follwoed by a new line
+* @str: The parameter to be accepted
+*/
+
+void _puts(char *str);
+{
+  for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
