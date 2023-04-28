d32e9723dfd03dec0f030c67782e124469721e52
Ethan Munsami <ethanmunsami@gmail.com>
2023-04-28 05:30:27 -0700

    Added 2-add_node.c" q it git

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * add_node - Fucntion adds a new node at the beginning of a list
+  *
+  * @head: The original linked list
+  * @str: The string to add to the node
+  *
+  * Return: The address of the new list or NULL if it failed
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	if (head != NULL && str != NULL)
+	{
+		temp = malloc(sizeof(list_t));
+		if (temp == NULL)
+			return (NULL);
+
+		temp->str = strdup(str);
+		temp->len = _strlen(str);
+		temp->next = *head;
+
+		*head = temp;
+
+		return (temp);
+	}
+
+	return (0);
+}
+
+/**
+  * _strlen - Fucntion returns the length of a string
+  *
+  * @s:The string
+  *
+  * Return: String length
+  */
+
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+
+	return (c);
+}
